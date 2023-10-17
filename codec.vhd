9c9e240554efc1264118c973cbc7484790000d3f
Guilherme Gonzaga <guilhermgonzaga@gmail.com>
2023-10-17 11:46:56 -0400

    Partial firmware read implementation

--- codec.vhd
+++ codec.vhd
@@ -15,3 +15,30 @@ entity codec is
 		codec_data_out: out std_logic_vector(7 downto 0)
 	);
 end entity;
+
+architecture behavioral of codec is
+	file firmware: text;
+begin
+
+	read_data: process is
+		variable instruction: line;
+		variable logvec: std_logic_vector(7 downto 0);
+	begin
+		file_open(firmware, firmware_filename, read_mode);
+
+		while not endfile(firmware) loop
+			readline(firmware, instruction);
+			for i in logvec'range loop
+				if instruction(i) = '1' then
+					logvec(i) := '1';
+				elsif instruction(i) = '0' then
+					logvec(i) := '0';
+				end if;
+				codec_data_out <= logvec;
+			end loop;
+		end loop;
+
+		wait;
+	end process;
+
+end architecture;
